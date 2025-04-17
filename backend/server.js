19859ef0cbe99785cb7b7824bf636281bb8fe124
Antonin-NICOLAS <antoninnicolas@icloud.com>
2025-04-17 18:52:16 +0200

    cookies

--- backend/server.js
+++ backend/server.js
@@ -15,19 +15,16 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
-//cors plugin
-app.use(cors({
+
+//cors
+const corsOptions = {
+    origin: process.env.FRONTEND_SERVER,
     credentials: true,
-    origin: process.env.FRONTEND_SERVER
-}))
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_SERVER);
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
 
 //routes
 app.use('/api/auth', AuthRoutes)
